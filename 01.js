fbe93bc9138d9679f44a86d6ac09136f539836c2
Lina Dang <lina.dang@shopee.com>
2018-12-16 13:20:13 +0800

    new checkout

--- 01.js
+++ 01.js
@@ -2,9 +2,7 @@ console.log(1);
 console.log(2);
 console.log(3);
 console.log('我是远程仓库新增的');
-<<<<<<< HEAD
 console.log('我是又添加到远程仓库的');
 console.log('我是又添加到远程仓库的2');53
-=======
 console.log('我是远程仓库新增的2');
->>>>>>> 1b2fcfd66fb765e11adfada8cc9848baae4fbe75
+console.log('我要checkout原来的版本');

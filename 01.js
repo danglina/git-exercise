09d8803209248248abcd2351d0797e7628eb4694
Lina Dang <lina.dang@shopee.com>
2019-01-02 18:03:36 +0800

    delete the cons

--- 01.js
+++ 01.js
@@ -1,9 +1,5 @@
 console.log(1);
 console.log(2);
 console.log(3);
-console.log('我是远程仓库新增的');
-console.log('我是又添加到远程仓库的');
-console.log('我是又添加到远程仓库的2');53
-console.log('我是远程仓库新增的2');
-console.log('我要checkout原来的版本');
-console.log('我要checkout原来的版本2');
+
+
